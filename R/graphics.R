03b772dd282c7537e821056b8a5148e4318ade53
Rory Nolan <rorynoolan@gmail.com>
2017-11-28 14:15:18 +0000

    Rearrange paper.

--- R/graphics.R
+++ R/graphics.R
@@ -17,11 +17,11 @@ display <- function(img, ...) {
   checkmate::assert_matrix(img)
   img %<>% {t(.[rev(seq_len(nrow(.))), ])}
   dots <- list(...)
-  dots[[1]] <- img
   if (! "axes" %in% names(dots)) dots$axes <- FALSE
   if (! "col" %in% names(dots)) {
     dots$col <- grDevices::grey.colors(999, start = 0, end = 1)
   }
+  dots %<>% c(list(img), .)
   do.call(fields::image.plot, dots)
 }
 
