319197be89f4954f9932e1b65e72e4f8e8001a23
Rory Nolan <rorynoolan@gmail.com>
2018-05-10 13:35:20 +0100

    Quell NOTEs on CRAN checks.

--- R/ijtiff.R
+++ R/ijtiff.R
@@ -1,6 +1,7 @@
 #' @useDynLib ijtiff
 #' @importFrom Rcpp sourceCpp
 #' @importFrom magrittr '%>%' '%<>%' '%T>%'
+#' @importFrom grDevices chull
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
